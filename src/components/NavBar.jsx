3c9337f7ddcc45a8e13a85010ae902c9df1187f4
michaelwang110 <michael.mhw1@gmail.com>
2022-06-24 22:48:05 +1000

    finished navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../assets/logo.png';
 import {FaBars, FaTimes} from 'react-icons/fa';
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(prev => !prev);
+
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -19,12 +22,12 @@ const NavBar = () => {
       </ul>
 
       {/* Hamburger */}
-      <div className='md:hidden z-10'>
-        <FaBars />
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* Mobile Menu */}
-      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>Home</li>
         <li className='py-6 text-4xl'>About</li>
         <li className='py-6 text-4xl'>Skills</li>
