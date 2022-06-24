be70ba767b4bff0b1e5c44edbef041350f394e79
michaelwang110 <michael.mhw1@gmail.com>
2022-06-25 06:54:54 +1000

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import Socials from './Socials';
 const Footer = () => {
   return (
-    <div name='footer' className='w-full h-fit bg-[#0a192f] text-gray-300 pt-52'>
+    <div name='footer' className='sm:flex w-full h-fit bg-[#0a192f] text-gray-300 pt-52'>
       {/* Container */}
-      <div className='flex'>
-        <div className='ml-5 text-sm'>
-          © 2022 All rights reserved. – Designed & Coded by Michael Wang
+        {/* desktop */}
+        <div className='sm:flex hidden ml-5 text-sm'>
+          <span className='text-gray-400'>© 2022 All rights reserved. –&nbsp;</span> Designed & Coded by Michael Wang
         </div>
-        <div className='ml-auto mr-5 mb-5'>
+        <div className='sm:flex hidden ml-auto mr-5 mb-5'>
           <Socials size={25}/>
         </div>
-      </div>
+
+        {/* mobile */}
+        <div className='sm:hidden flex m-auto mr-5 mb-5 justify-center'>
+          <Socials size={25}/>
+        </div>
+        <div className='sm:hidden flex text-sm justify-center pb-5'>
+          <span className='text-gray-400'>© 2022 All rights reserved. –&nbsp;</span> Designed & Coded by Michael Wang
+        </div>
     </div>
   )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,36 +1,31 @@
 import React from 'react'
-import { Document, Page, pdfjs } from 'react-pdf';
+import resume_img from '../assets/michael_wang_resume.png'; 
 import resume from '../assets/michael_wang_resume.pdf';
 import { FiDownload } from 'react-icons/fi';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
   return (
     <div name='resume' className='w-full h-fit bg-[#0a192f] text-gray-300'>
-      <div className='lg:mb-0 md:mb-4 sm:mb-16 mb-8 w-full '>
-        {/* Container */}
-        <div className='flex flex-col justify-center items-center w-full h-full'>
-          <div className='max-w-[1000px] w-full px-4 pt-52'>
-            {/* Title */}
-            <div className='pb-12 text-center'>
-              <p className='sm:text-4xl text-2xl font-bold inline border-b-4 border-pink-600'>Resume</p>
-            </div>
-            {/* Content */}
-            <div className='flex justify-center'>
-              <Document file={resume}>
-                <Page height={800} pageNumber={1} />
-              </Document>
-            </div>
-            <div className='flex justify-center items-center'>
-              <a 
-                className='text-gray-200 bg-pink-600 font-size text-2xl hover:bg-pink-700 rounded-full mt-6  duration-500'
-                href={resume}
-                download
-              >
-                <button className='p-4'><FiDownload /></button>
-              </a>
-            </div>
+      {/* Container */}
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full px-4 pt-52'>
+          {/* Title */}
+          <div className='pb-12 text-center'>
+            <p className='sm:text-4xl text-2xl font-bold inline border-b-4 border-pink-600'>Resume</p>
+          </div>
+          {/* Content */}
+          <div className='flex justify-center'>
+            <img class='mx-auto h-[80%]' src={resume_img} alt='Resume'/>
+          </div>
+          <div className='flex justify-center items-center'>
+            <a 
+              className='text-gray-200 bg-pink-600 font-size text-2xl hover:bg-pink-700 rounded-full mt-6  duration-500'
+              href={resume}
+              download
+            >
+              <button className='p-4'><FiDownload /></button>
+            </a>
           </div>
         </div>
       </div>
