a8c5c8604a05df3104d1e99010536a909b918688
michaelwang110 <michael.mhw1@gmail.com>
2022-06-24 22:40:59 +1000

    set up styling for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,23 +8,9 @@ const NavBar = () => {
       <div>
         <img src={Logo} alt='Logo Image' style={{width: '90px'}}/>
       </div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Work</li>
-          <li>Contact</li>
-        </ul>
-      </div>
 
-      {/* Hamburger */}
-      <div className='hidden'>
-        <FaBars />
-      </div>
-
-      {/* Mobile Menu */}
-      <ul className='hidden'>
+      {/* Menu */}
+      <ul className='hidden md:flex'>
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
@@ -32,8 +18,22 @@ const NavBar = () => {
         <li>Contact</li>
       </ul>
 
+      {/* Hamburger */}
+      <div className='md:hidden z-10'>
+        <FaBars />
+      </div>
+
+      {/* Mobile Menu */}
+      <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+        <li className='py-6 text-4xl'>Home</li>
+        <li className='py-6 text-4xl'>About</li>
+        <li className='py-6 text-4xl'>Skills</li>
+        <li className='py-6 text-4xl'>Work</li>
+        <li className='py-6 text-4xl'>Contact</li>
+      </ul>
+
       {/* Social Icons */}
-      <div></div>
+      <div className='hidden'></div>
     </div>
   )
 }
