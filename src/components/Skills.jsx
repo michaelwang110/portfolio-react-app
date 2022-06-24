81221dd99f274adb7641b291bc1638f931a74fbb
michaelwang110 <michael.mhw1@gmail.com>
2022-06-25 05:08:16 +1000

    finished skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import react from '../assets/react.png';
 import nextjs from '../assets/nextjs.png';
+import dotnetcore from '../assets/dotnetcore.png';
+import django from '../assets/django.png';
+import apollo from '../assets/apollo.png';
+import graphql from '../assets/graphql.png';
+import postgresql from '../assets/postgresql.png';
+import html from '../assets/html.png';
+import css from '../assets/css.png';
+import typescript from '../assets/typescript.png';
 
 const Skills = () => {
   return (
@@ -19,40 +27,40 @@ const Skills = () => {
               <p className='mt-1 mb-2'>ReactJS</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={nextjs} alt='react' />
+              <img className='mt-5 mb-5 w-20 mx-auto' src={nextjs} alt='react' />
               <p className='mt-1 mb-2'>NextJS</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-2 w-20 mx-auto' src={dotnetcore} alt='react' />
+              <p className='mt-1 mb-2'>.Net Core</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-2 w-20 mx-auto' src={django} alt='react' />
+              <p className='mt-1 mb-2'>Django Rest</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-2 w-20 mx-auto' src={apollo} alt='react' />
+              <p className='mt-1 mb-2'>Apollo Client</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-2 w-20 mx-auto' src={graphql} alt='react' />
+              <p className='mt-1 mb-2'>GraphQL</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-2 w-20 mx-auto' src={postgresql} alt='react' />
+              <p className='mt-1 mb-2'>PostgreSQL</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-4 w-16 mx-auto' src={html} alt='react' />
+              <p className='mt-4 mb-1'>HTML</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-4 w-16 mx-auto' src={css} alt='react' />
+              <p className='mt-4 mb-1'>CSS</p>
             </div>
             <div className='rounded-lg hover:scale-110 border border-pink-800 hover:border-pink-500 duration-500'>
-              <img className='mt-2 w-20 mx-auto' src={react} alt='react' />
-              <p className='mt-1 mb-2'>React</p>
+              <img className='mt-4 w-16 mx-auto' src={typescript} alt='react' />
+              <p className='mt-4 mb-1'>TypeScript</p>
             </div>
           </div>
         </div>
