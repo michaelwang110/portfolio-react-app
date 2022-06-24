9b833340f63d54b82cb34c10836855f9bf644a51
michaelwang110 <michael.mhw1@gmail.com>
2022-06-25 02:05:35 +1000

    added an animated background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import {HiArrowNarrowRight} from 'react-icons/hi';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-      Home
+    <>
+
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+
+      {/* Container */}
+
+      <div class="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        {/* source: https://www.youtube.com/watch?v=Tmkr2kKUEgU */}
+        <div class=" w-full max-w-[1000px] z-10">
+          <div class="absolute top-36 -left-4 w-36 h-36 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob"></div>
+          <div class="absolute top-0 -right-4 w-96 h-96 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-2000"></div>
+          <div class="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-100 animate-blob animation-delay-4000"></div>
+        </div>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ffffff] z-20'>
+          Hi there
+          <span class="waving-hand">👋</span>
+        </h1>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ffffff] z-20'>
+          I'm {<span className='text-4xl sm:text-7xl font-bold text-[#eee955]'>Michael Wang</span>}
+          , a Software Engineer based in Sydney.
+        </h1>
+      </div>
     </div>
+    </>
   )
 }
 
-export default Home
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,9 +11,9 @@ const NavBar = () => {
   const handleClick = () => setNav(prev => !prev);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-50'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{width: '90px'}}/>
+        <img src={Logo} alt='Logo' style={{width: '90px'}}/>
       </div>
 
       {/* Menu */}
